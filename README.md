9de895c80dbbfbb0b40694be5d2ce4e0053f57ac
Austinmoore1492 <austinmoore1492@yahoo.com>
2020-01-13 17:41:39 -0600

    added build from R-Portfolio

--- README.md
+++
@@ -1,7 +0,0 @@
-# Portfolio
-
-This is my personal Website hosted on GitHub pages.
-
-## Live Site
-
-[Austin's Portfolio](https://austinmoore1492.github.io/)
---
+++ precache-manifest.4f17aeffb2d2e19d74a15b8c938df99b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1858d70fed7b13ce13eee830e518a77a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19ca680ba999fac11bda",
+    "url": "./static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "9061755404b66557fc26",
+    "url": "./static/css/main.000bf9d6.chunk.css"
+  },
+  {
+    "revision": "19ca680ba999fac11bda",
+    "url": "./static/js/2.12d0a257.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.12d0a257.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9061755404b66557fc26",
+    "url": "./static/js/main.714b1eba.chunk.js"
+  },
+  {
+    "revision": "547454f6e3de6ab13ac8",
+    "url": "./static/js/runtime-main.934a30fb.js"
+  }
+]);
