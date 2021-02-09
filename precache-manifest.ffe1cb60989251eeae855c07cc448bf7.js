c254fd255f809c5281491c76581d140b2a99e780
Austin Leaverton <37962463+Austinmoore1492@users.noreply.github.com>
2021-02-09 12:51:05 -0600

    Updated Project Section

---
+++ precache-manifest.ffe1cb60989251eeae855c07cc448bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8c78b85bc247b7026da734a98c2741",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d317a5adb19efdeca876",
+    "url": "./static/css/main.59a83c59.chunk.css"
+  },
+  {
+    "revision": "d83b8dce640e98bfe14d",
+    "url": "./static/js/2.9416c042.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.9416c042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d317a5adb19efdeca876",
+    "url": "./static/js/main.95fc8821.chunk.js"
+  },
+  {
+    "revision": "03575165036da8444cf4",
+    "url": "./static/js/runtime-main.8f28b43c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a162562c57be1dc9002280784fcae4b2.js"
+  "./precache-manifest.ffe1cb60989251eeae855c07cc448bf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
